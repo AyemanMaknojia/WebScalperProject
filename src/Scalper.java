08abb9190a1f5d1f427e20d01aaafb4874d4249a
100000561 <100000561@ccisd.net>
2023-02-16 10:40:04 -0600

    made scalper class with Selenium framework

---
+++ src/Scalper.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scalper {
+    public static void main(String[] args)
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\100000561\\Downloads\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+    }
+}
