d8e6daf922888ca9ec34f0da434fa33d92a39ef5
ayemanmaknojia <100000561@ccisd.net>
2023-03-09 09:53:56 -0600

    fixed spelling errors in comments

--- src/main/java/Scalper.java
+++ src/main/java/Scalper.java
@@ -13,9 +13,9 @@ public class Scalper {
         //get the amazon link to open on the web driver
         //driver.get("https://www.amazon.com/Keeper-Lost-Cities-Collection-Messenger/dp/9124126144/ref=sr_1_1?crid=3KPNGMUM6VRBS&keywords=keeper+of+the+lost+cities+series&qid=1677516487&s=books&sprefix=keeper+of+lost+ci%2Cstripbooks%2C92&sr=1-1");
         driver.get("https://www.amazon.com/Xbox-X/dp/B08H75RTZ8/ref=sr_1_2?crid=1GCC0N20FNF09&keywords=x%2Bboxes%2Bseries%2Bx&qid=1678372259&s=videogames&sprefix=x%2Bbox%2Cvideogames%2C107&sr=1-2&th=1");
-        //stop thread for 1 second
+        //stop program for 1 second
         Thread.sleep(1000);
-        // while loop will keep going until it reaches the iof statement
+        // while loop will keep going until it reaches the if statement
         boolean y = true;
         while (y) {
             //if submit.add-to-cart is present in the page source then press the add to cart button and quit the driver
@@ -26,9 +26,8 @@ public class Scalper {
                 //sleep for 3 seconds, then quit the driver
                 Thread.sleep(500);
                 driver.quit();
-                //break;
             }
-            //driver will refresh until there is an add to cart button present, or the user manually switches it off.
+            //driver will refresh until there is an add to cart button present, or the user manually switches the driver off.
             else
             {
                 Thread.sleep(500);
